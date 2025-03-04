b519fab0d9fde0804d1a9f370f25332f60bcda1e
willians henrique <willianshenrique94@gmail.com>
2025-03-04 20:52:52 -0300

    Initial commit

---
+++ src/test/java/me/dio/santander_dev_web_2023/SantanderDevWeb2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander_dev_web_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeb2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
